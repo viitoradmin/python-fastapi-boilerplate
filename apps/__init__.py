17cfa55dabb73d27f03d207a8ec57a3577c96276
joshijhanvi <jhanvi.joshi@viitor.cloud>
2023-10-17 14:06:11 +0530

    fix: changes in versioning for API's and in structure.

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,16 +1,13 @@
+from config import cors
 from fastapi import FastAPI
 from fastapi_versioning import VersionedFastAPI
-
-from apps.api.auth.v1.view import router
-from apps.api.auth.v2.view import authrouter
-from config import cors
+from apps.api.auth.view import defaultrouter
 
 # Create app object and add routes
 app = FastAPI(title="Python FastAPI boilerplate", middleware=cors.middleware)
 
 # define router for different version
-app.include_router(router, prefix="/v1", tags=["v1"]) # router for version 1
-app.include_router(authrouter, prefix="/v2", tags=["v2"]) # router for version 2
+app.include_router(defaultrouter) # router for version 1
 
-# # # Define version to specify version related API's.
+# Define version to specify version related API's.
 app = VersionedFastAPI(app, version_format="{major}", prefix_format="/v{major}", enable_latest=True)
--- apps/api/auth/view.py
+++ apps/api/auth/view.py
@@ -1,24 +1,46 @@
 from fastapi import status
 from fastapi_utils.cbv import cbv
-from fastapi_utils.inferring_router import InferringRouter
-
+from fastapi import APIRouter
 from apps.constant import constant
+from fastapi_versioning import version
+from fastapi_utils.inferring_router import InferringRouter
 from apps.utils.standard_response import StandardResponse
 
 ## Load API's
-router = InferringRouter()
+defaultrouter = APIRouter()
 
 ## Define API's here
-@cbv(router)
+@cbv(defaultrouter)
 class UserCrudApi():
     """This class is for user's CRUD operation with version 1 API's"""
     
-    @router.get('/list/user')
+    @defaultrouter.get('/v1/list/user')
+    @version(1)
     async def list_user(self):
         """This API is for list user.
         """
         try:
-            response = "Hello there, welcome to fastapi bolierplate"
-            return response
+            data = "Hello there, welcome to fastapi bolierplate"
+            return data
+        except Exception as e:
+            return StandardResponse(False, status.HTTP_400_BAD_REQUEST, None, constant.ERROR_MSG)
+    
+    @defaultrouter.post('/v1/create/user')
+    @version(1)
+    async def list_user(self):
+        """This API is for list user.
+        """
+        try:
+            data = "Hello there, welcome to fastapi bolierplate"
+            return data
+        except Exception as e:
+            return StandardResponse(False, status.HTTP_400_BAD_REQUEST, None, constant.ERROR_MSG)
+        
+    @defaultrouter.get("/v2/list")
+    @version(2)
+    async def get_list(self):
+        try:
+            response = { "data": "User's list data" }
+            return StandardResponse(True, status.HTTP_200_OK, response, constant.STATUS_SUCCESS)
         except Exception as e:
             return StandardResponse(False, status.HTTP_400_BAD_REQUEST, None, constant.ERROR_MSG)
